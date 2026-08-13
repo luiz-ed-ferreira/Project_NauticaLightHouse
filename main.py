076354d30a990a2dd733f49ed5f20d4bf620e909
Luiz Eduardo Ferreira <145693602+luiz-ed-ferreira@users.noreply.github.com>
2026-08-13 18:21:50 -0300

    Fix print statement for clarity in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Inicie por aqui
-print("Esse arquivo foi criado para orientar a ordem que os demais arquivos notebooks devem ser acessados e quais arquivos eles geram:")
+print("Esse arquivo foi criado para orientar a ordem que os demais arquivos e notebooks devem ser acessados e quais arquivos eles geram:")
 print("Q1 - EDA.ipynb > orders.db")
 print("Q2 - Schemas.ipynb > schemas.sql")
 print("Q3 - Load.ipynb")
